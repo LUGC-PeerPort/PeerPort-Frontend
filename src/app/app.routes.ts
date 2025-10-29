6b13a231df51f43ec90c4299d97ccc3823059e47
saadh <shzaidi@lakeheadu.ca>
2025-10-29 13:46:15 -0400

    removed logout components and page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 import { CourseComponent } from './components/course/course.component';
 import { AssignmentComponent } from './components/assignment/assignment.component';
  import { LoginComponent } from './components/login/login.component';
- import { LogoutComponent } from './components/logout/logout.component';
 import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
@@ -11,6 +10,5 @@ export const routes: Routes = [
     {path: 'course', component: CourseComponent},
     {path: 'assignments', component: AssignmentComponent},
      {path: 'login', component: LoginComponent},
-     {path: 'logout', component: LogoutComponent},
     {path: 'register', component: RegisterComponent}
 ];
