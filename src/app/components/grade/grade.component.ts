1bf1496a4ea118498b40607b9bd9b3ec21f5c2ae
CommanderKV <kylervisser@gmail.com>
2025-11-24 01:42:23 -0500

    Refactored grades

--- src/app/components/grade/grade.component.ts
+++ src/app/components/grade/grade.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CourseService } from '../../services/course.service';
+import { CourseService, assignment, submission, grade } from '../../services/course.service';
 import { DataService } from '../../services/data.service';
 import { AuthService } from '../../services/auth.service';
 import { NgFor,NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+interface submissionWithGrade extends submission {
+  grade: grade | null;
+}
+interface assignmentWithSubmissions extends assignment {
+  submissions: submissionWithGrade[];
+}
 
 @Component({
   selector: 'app-grade',
@@ -14,159 +21,222 @@ import { FormsModule } from '@angular/forms';
 })
 export class GradeComponent implements OnInit {
 
-  GRADES: any;
-  COURSES: any;
-  ASSIGNMENTS:any;
-  SUBMISSIONS: any = {};
-  courseId!: string;
-  name: string | undefined;
-  courseCode: string | undefined;
-  isOpen: boolean | undefined;
-  description: string | undefined;
-  startDate: string | undefined;
-  endDate: string | undefined;
+  // Variables
+  user: { userId: string, userRole: string } | null = null;
+  student: boolean = true;
+  assignments: assignmentWithSubmissions[] | [] = [];
+  grades: grade[] | [] = [];
+  averageGrade: number = 0;
+  courseId: string = "";
+  courseName: string = "";
 
-  User: {userId: string, userRole: string} | null = null;
-  userId!: string;
-  userRole: string | undefined;
+  // Grade variables
+  minScore: number = 0;
+  maxScore: number = 0;
+  achievedScore: number = 0;
+  weight: number = 0;
+  gradeId: string = "";
+  submissionId: string = "";
 
-  userGrades: any;
-  userAverageGrade: any;
-
-  courseAverageGrade:any;
-
-  isGradePopupOpen = false;
-
-  constructor(private route: ActivatedRoute, private CourseService: CourseService, private dataService: DataService, private authService: AuthService){}
-  currentSubmissionId!: string; //change
-  openCloseGradePopup(submissionId?: string){
-    if(submissionId){//change 3 lines
-      this.currentSubmissionId = submissionId;
-    }
-    this.isGradePopupOpen = !this.isGradePopupOpen;
-  }
+  // Popup controls
+  isPopUpOpen: boolean = false;
+  activity: Function = () => {};
   
 
+  constructor(private route: ActivatedRoute, private courses: CourseService, private dataService: DataService, private authService: AuthService){}
 
-//GET
-  getAllGradesForCourse(courseId:string):void{
-    this.CourseService.getAllGradesForCourse(courseId).subscribe(response=>{
-      this.GRADES = response;
-    });
-  }
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      // Get the courseId from the route parameters
+      this.courseId = params['id'];
 
-  getAllGradesForUser(courseId:string, userId:string):void{
-    this.CourseService.getAllGradesForUserByCourse(courseId,userId).subscribe(response=>{
-      this.userGrades = response;
-    });
-  }
-
-  getAverageGradeOfCourse(courseId:string):void{
-    this.CourseService.getAverageGradeForCourse(courseId).subscribe((response:any)=>{
-      console.log(response);
-      this.courseAverageGrade = response.grade;
-    });
-  }
-
-  getAverageGradeForUser(courseId:string, userId:string):void{
-    this.CourseService.getAverageGradeForUser(courseId,userId).subscribe((response:any)=>{
-      this.userAverageGrade = response.grade;
-    });
-  }
-
-  getAllAssignments(courseId:string){
-    this.CourseService.getAllAssignmentsByCourseId(courseId).subscribe(response =>{
-      this.ASSIGNMENTS = response;
-
-      this.ASSIGNMENTS?.forEach((a: any) => this.getAllSubmissionsForAssignment(a.assignmentId));
-    })
-  }
-
-  getAllSubmissionsForAssignment(assignmentId:string){
-    this.CourseService.getAllSubmissionsForAssignment(assignmentId).subscribe(response =>{
-      this.SUBMISSIONS[assignmentId] = response;
-    })
-  }
-
-  //CREATE
-  createGrade(courseId:string, userId:string, assignmentSubmissionId:string, minScore:number,maxScore:number,achievedScore:number,weight:number):void{
-      const grade ={
-        minScore: minScore,
-        maxScore:maxScore,
-        achievedScore:achievedScore,
-        weight: weight
-      }
-
-    this.CourseService.createGradeForAssignment(courseId, userId, assignmentSubmissionId, grade).subscribe(response=>{
-      this.GRADES.push(response);
-    });
-  }
-
-  //EDIT
-  editGrade(gradeId:string, minScore:number,maxScore:number, achievedScore:number, weight:number):void{
-    const editedGrade = {
-     minScore: minScore,
-     maxScore: maxScore,
-     achievedScore: achievedScore,
-     weight: weight
-    }
-
-    this.CourseService.editGrade(gradeId, editedGrade).subscribe(response=>{
-      const index = this.GRADES.findIndex((g:any) => g.gradeId === gradeId);
-      if (index !== -1) {
-        this.GRADES[index] = { ...this.GRADES[index], ...response };
-      }
-    });
-  }
-  //edit popup - added this - change
-  isEditPopupOpen = false;
-  //selectedGradeId: string | null = null; //change
-  currentGradeId!: string; //change
-
-  openEditPopup(gradeId: string) {
-    //this.selectedGradeId = gradeId; //change
-    this.currentGradeId = gradeId; //change
-    this.isEditPopupOpen = true;
-  }
-  closeEditPopup() {
-    this.isEditPopupOpen = false;
-    //this.selectedGradeId = null;
-  }
-  //DELETE
-  deleteGrade(gradeId:string):void{
-    this.CourseService.deleteGrade(gradeId).subscribe(response=>{
-      console.log("Grade deleted.");
-    })
-  }
-
- ngOnInit(): void {
-  this.authService.currentUser().subscribe(userResponse => {
-    this.User = userResponse as { userId: string, userRole: string };
-    if (this.User) {
-      this.userId = this.User.userId;
-      this.userRole = this.User.userRole;
-
-      this.route.params.subscribe(params => {
-        this.courseId = params['id'];
-        this.dataService.changeMessage(this.courseId);
-
-        this.CourseService.getCourseById(this.courseId).subscribe(courseResponse => {
-          this.COURSES = courseResponse;
-        });
-
-        this.getAllAssignments(this.courseId);
-
-        this.getAverageGradeOfCourse(this.courseId);
-
-        this.getAverageGradeForUser(this.courseId, this.userId);
-        this.getAllGradesForUser(this.courseId, this.userId);
-
-        this.getAllGradesForCourse(this.courseId);
+      // Get the course name
+      this.courses.getCourseById(this.courseId).subscribe(courseResponse => {
+        this.courseName = courseResponse.name;
       });
-    }
-  });
-}
 
+      // Give the nav the courseId
+      this.dataService.changeMessage(this.courseId);
+
+      // Get the user info
+      this.authService.currentUser().subscribe(userResponse => {
+        this.user = userResponse as { userId: string, userRole: string };
+
+        // If the user is not a student
+        if (this.user.userRole !== "ff123156-7773-47f5-b5df-0ef54f864f8d") {
+          this.student = false;
+
+          // Get all grades for the course
+          this.getAllGradesForCourse();
+
+          // Get all assignments for the course
+          this.getAllAssignments();
+        } else {
+          // Get all the grades for this course
+          this.getAllGradesForUser();
+
+          // Get all the assignments for this course
+          this.getAllAssignments();
+        }
+
+        // Get the average grade for this course
+        this.getAverageGradeOfCourse();
+      });
+    });
   }
+
+  // Get all the grades for a course
+  getAllGradesForCourse(): void {
+    this.courses.getAllGradesForCourse(this.courseId).subscribe(response => {
+      if (!Array.isArray(response)) {
+        window.alert("Error encountered: " + response);
+        return;
+      }
+
+      this.grades = response;
+    });
+  }
+
+  // Get all the grades for a user in a course
+  getAllGradesForUser(): void {
+    this.courses.getAllGradesForUserByCourse(this.courseId, this.user!.userId).subscribe(response => {
+      if (!Array.isArray(response)) {
+        window.alert("Error encountered: " + response);
+        return;
+      }
+
+      this.grades = response;
+    });
+  }
+
+  // Get all assignments for a course
+  getAllAssignments(): void {
+    this.courses.getAllAssignmentsByCourseId(this.courseId).subscribe(response => {
+      if (!Array.isArray(response)) {
+        window.alert("Error encountered: " + response);
+        return;
+      }
+
+      const assignments = response;
+
+      // For each assignment, get the submissions
+      for (const assignment of assignments) {
+        this.getAllSubmissionsForAssignment(assignment);
+      };
+
+      console.log(assignments);
+      this.assignments = assignments as assignmentWithSubmissions[];
+    });
+  }
+
+  // Get all submissions for an assignment
+  getAllSubmissionsForAssignment(assignment: assignment): void {
+    this.courses.getAllSubmissionsForAssignment(assignment.assignmentId).subscribe(response => {
+      // If the response is not an array, treat it as an error object
+      if (!Array.isArray(response)) {
+        window.alert("Error encountered: " + response);
+        return;
+      }
+
+      // For each submission, find the corresponding grade
+      (assignment as assignmentWithSubmissions).submissions = response.map((submission: submission): submissionWithGrade => {
+        const grade = this.grades.find((g: grade) => g.assignmentSubmissionId === submission.assignmentSubmissionId);
+        (submission as submissionWithGrade).grade = grade ?? null;
+
+        return submission as submissionWithGrade;
+      });
+    });
+  }
+
+  // Get the average grade of a course
+  getAverageGradeOfCourse(): void {
+    this.courses.getAverageGradeForCourse(this.courseId).subscribe(response => {
+      this.averageGrade = response.grade;
+    });
+  }
+
+  // Create the grade
+  createGrade(): void {
+    // Define the grade using the component's properties
+    const grade = {
+      minScore:       Number(this.minScore),
+      maxScore:       Number(this.maxScore),
+      achievedScore:  Number(this.achievedScore),
+      weight:         Number(this.weight)
+    };
+
+    // Create the grade using the CourseService
+    this.courses.createGradeForAssignment(this.courseId, this.user!.userId, this.submissionId, grade).subscribe(response => {
+      // Handle the response
+      console.log("Grade created:", response);
+    });
+  }
+
+  // Updates the grade
+  updateGrade(): void {
+    // Define the updated grade using the component's properties
+    const updatedGrade = {
+      minScore:       Number(this.minScore),
+      maxScore:       Number(this.maxScore),
+      achievedScore:  Number(this.achievedScore),
+      weight:         Number(this.weight)
+    };
+
+    // Update the grade using the CourseService
+    this.courses.editGrade(this.gradeId, updatedGrade).subscribe(response => {
+      // Handle the response
+      console.log("Grade updated:", response);
+    });
+  }
+
+  // Deletes the grade
+  deleteGrade(submissionId: string): void {
+    // Get the gradeId for this submission
+    const gradeId = this.getGradeId(submissionId);
+    if (!gradeId) {
+      console.log("No grade found for submission:", submissionId);
+      return;
+    }
+
+    // Delete the grade using the CourseService
+    this.courses.deleteGrade(gradeId).subscribe(response => {
+      // Handle the response
+      console.log("Grade deleted:", response);
+    });
+  }
+
+  // Gets the gradeId for a submission
+  getGradeId(submissionId:string): string | null {
+    const grade = this.grades.find((g: any) => g.assignmentSubmissionId === submissionId);
+    return grade ? grade.gradeId : null;
+  }
+
+  // Toggles the popup and sets variables
+  togglePopUp(submissionId: string): void {
+    // Set the submissionId
+    this.submissionId = submissionId;
+
+    // Get the grade Id for this submission
+    const grade = this.grades.find((g: grade) => g.assignmentSubmissionId === submissionId);
+    this.gradeId = grade?.gradeId ?? "";
+    
+    // If a grade exists set the details of the grade
+    if (grade) {
+      this.minScore = grade.minScore;
+      this.maxScore = grade.maxScore;
+      this.achievedScore = grade.achievedScore;
+      this.weight = grade.weight;
+    }
+
+    // Toggle the popup
+    this.isPopUpOpen = !this.isPopUpOpen;
+  }
+
+  // Calculates the grade percentage
+  calculateGrade(grade: grade | null): number | null {
+    if (!grade) return null;
+    return (grade.achievedScore - grade.minScore) / (grade.maxScore - grade.minScore) * 100;
+  }
+}
 
 
